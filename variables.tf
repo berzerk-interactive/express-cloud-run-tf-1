70dec21ca7c291cbe2ca380a574e0a66eb1afb09
leeblazek <leeblazek@gmail.com>
2022-07-15 11:16:17 -0600

    files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "europe-west1"
+}
+
+variable "github_repository" {
+    default = "my-github-repository"
+}
+
+variable "github_owner" {
+    default = "my-github-username"
+}
+
+variable "github_branch" {
+    default = "^master$"
+}
